0da0f4be8cdcaa4355dfae21cdb697bd3f4f8338
Janik Besendorf <janik@besendorf.org>
2026-06-19 17:26:56 +0200

    Fix acquisition ZIP integrity checks

--- acquisition/secure.go
+++ acquisition/secure.go
@@ -18,20 +18,98 @@ import (
 )
 
 func createZipFile(sourceDir, zipPath string) error {
-	zipFile, err := os.Create(zipPath)
+	sourceAbs, err := filepath.Abs(sourceDir)
+	if err != nil {
+		return fmt.Errorf("failed to resolve source directory: %v", err)
+	}
+	zipAbs, err := filepath.Abs(zipPath)
+	if err != nil {
+		return fmt.Errorf("failed to resolve ZIP path: %v", err)
+	}
+
+	zipFile, err := os.OpenFile(zipPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
 	if err != nil {
 		return fmt.Errorf("failed to create ZIP file: %v", err)
 	}
-	defer zipFile.Close()
 
 	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
 
-	// Use AddFS to add the entire directory
-	fsys := os.DirFS(sourceDir)
-	err = zipWriter.AddFS(fsys)
+	err = filepath.WalkDir(sourceDir, func(filePath string, dirEntry os.DirEntry, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+		if dirEntry.IsDir() {
+			return nil
+		}
+
+		fileAbs, err := filepath.Abs(filePath)
+		if err != nil {
+			return err
+		}
+		if fileAbs == zipAbs {
+			return nil
+		}
+
+		fileInfo, err := dirEntry.Info()
+		if err != nil {
+			return err
+		}
+		if !fileInfo.Mode().IsRegular() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(sourceAbs, fileAbs)
+		if err != nil {
+			return err
+		}
+		header, err := zip.FileInfoHeader(fileInfo)
+		if err != nil {
+			return err
+		}
+		header.Name = filepath.ToSlash(relPath)
+		header.Method = zip.Deflate
+
+		writer, err := zipWriter.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Open(filePath)
+		if err != nil {
+			return err
+		}
+
+		_, copyErr := io.Copy(writer, file)
+		closeErr := file.Close()
+		if copyErr != nil {
+			return copyErr
+		}
+		return closeErr
+	})
 	if err != nil {
+		_ = zipWriter.Close()
+		_ = zipFile.Close()
 		return fmt.Errorf("failed to add directory to ZIP: %v", err)
+	}
+
+	if err := zipWriter.Close(); err != nil {
+		_ = zipFile.Close()
+		return fmt.Errorf("failed to close ZIP writer: %v", err)
+	}
+	if err := zipFile.Sync(); err != nil {
+		_ = zipFile.Close()
+		return fmt.Errorf("failed to sync ZIP file: %v", err)
+	}
+	if err := zipFile.Close(); err != nil {
+		return fmt.Errorf("failed to close ZIP file: %v", err)
+	}
+
+	reader, err := zip.OpenReader(zipPath)
+	if err != nil {
+		return fmt.Errorf("failed to verify ZIP file: %v", err)
+	}
+	if err := reader.Close(); err != nil {
+		return fmt.Errorf("failed to close ZIP verification reader: %v", err)
 	}
 
 	return nil
@@ -65,6 +143,7 @@ func (a *Acquisition) StoreSecurely() error {
 
 	err = createZipFile(a.StoragePath, zipFilePath)
 	if err != nil {
+		_ = os.Remove(zipFilePath)
 		return err
 	}
 
@@ -85,34 +164,57 @@ func (a *Acquisition) StoreSecurely() error {
 	if err != nil {
 		return err
 	}
-	defer zipFile.Close()
+	zipFileClosed := false
+	defer func() {
+		if !zipFileClosed {
+			zipFile.Close()
+		}
+	}()
 
 	encFileName := fmt.Sprintf("%s.age", zipFileName)
 	encFilePath := filepath.Join(cwd, encFileName)
-	encFile, err := os.OpenFile(encFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o600)
+	tmpEncFilePath := encFilePath + ".tmp"
+	encFile, err := os.OpenFile(tmpEncFilePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
 	if err != nil {
 		return fmt.Errorf("unable to create encrypted file: %v", err)
 	}
-	defer encFile.Close()
+	defer os.Remove(tmpEncFilePath)
 
 	w, err := age.Encrypt(encFile, recipient)
 	if err != nil {
+		encFile.Close()
 		return fmt.Errorf("failed to create encrypted file: %v", err)
 	}
 
 	_, err = io.Copy(w, zipFile)
 	if err != nil {
+		w.Close()
+		encFile.Close()
 		return fmt.Errorf("failed to write to encrypted file: %v", err)
 	}
 
 	if err := w.Close(); err != nil {
+		encFile.Close()
 		return fmt.Errorf("failed to close encrypted file: %v", err)
+	}
+	if err := encFile.Sync(); err != nil {
+		encFile.Close()
+		return fmt.Errorf("failed to sync encrypted file: %v", err)
+	}
+	if err := encFile.Close(); err != nil {
+		return fmt.Errorf("failed to close encrypted output file: %v", err)
+	}
+	if err := os.Rename(tmpEncFilePath, encFilePath); err != nil {
+		return fmt.Errorf("failed to move encrypted file into place: %v", err)
 	}
 
 	log.Infof("Acquisition successfully encrypted at %s", encFilePath)
 
 	// TODO: we should securely wipe the files.
-	zipFile.Close()
+	if err := zipFile.Close(); err != nil {
+		return fmt.Errorf("failed to close the unencrypted compressed archive: %v", err)
+	}
+	zipFileClosed = true
 	err = os.Remove(zipFilePath)
 	if err != nil {
 		return fmt.Errorf("failed to delete the unencrypted compressed archive: %v", err)
