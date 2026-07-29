a50bff0f5c31e9f0856ce540dab33bc8166a6284
Janik Besendorf <janik@besendorf.org>
2026-07-29 16:44:53 +0200

    Harden package list parsing

---
+++ adb/packages_test.go
@@ -0,0 +1,49 @@
+package adb
+
+import "testing"
+
+func TestParsePackageListWithInstaller(t *testing.T) {
+	out := "package:org.example installer=com.android.vending uid:10123\n"
+	entries, err := parsePackageList(out, true)
+	if err != nil {
+		t.Fatalf("parsePackageList() error = %v", err)
+	}
+	if len(entries) != 1 {
+		t.Fatalf("entry count = %d, want 1", len(entries))
+	}
+	if entries[0].name != "org.example" || entries[0].installer != "com.android.vending" || entries[0].uid != 10123 {
+		t.Fatalf("entry = %+v", entries[0])
+	}
+}
+
+func TestParsePackageListWithoutInstaller(t *testing.T) {
+	entries, err := parsePackageList("package:org.example uid:10123", false)
+	if err != nil {
+		t.Fatalf("parsePackageList() error = %v", err)
+	}
+	if len(entries) != 1 || entries[0].installer != "" || entries[0].uid != 10123 {
+		t.Fatalf("entries = %+v", entries)
+	}
+}
+
+func TestParsePackageListHandlesEmptyOutput(t *testing.T) {
+	entries, err := parsePackageList("", true)
+	if err != nil {
+		t.Fatalf("parsePackageList() error = %v", err)
+	}
+	if len(entries) != 0 {
+		t.Fatalf("entries = %+v, want none", entries)
+	}
+}
+
+func TestParsePackageListRejectsMalformedPackageRecords(t *testing.T) {
+	for _, line := range []string{
+		"package:org.example",
+		"package:org.example unexpected uid:10123",
+		"package:org.example installer=com.android.vending uid:not-a-number",
+	} {
+		if _, err := parsePackageList(line, true); err == nil {
+			t.Fatalf("parsePackageList(%q) error = nil", line)
+		}
+	}
+}
